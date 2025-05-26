4a8483c9f7e6b1fa30a9983df4721bdacaa6b4c9
Shushma-Onefinitylabs <shushma@onefinitylabs.com>
2025-05-26 14:16:40 +0000

    m

--- src/print.sh
+++ src/print.sh
@@ -3,27 +3,29 @@
 # Check if the required commands are available
 command -v echo >/dev/null 2>&1 || { echo "Error: 'echo' command not found."; exit 1; }
 command -v expr >/dev/null 2>&1 || { echo "Error: 'expr' command not found."; exit 1; }
-# Initialize the counter
+#!/bin/bash
+
+# Initialize counter
 i=1
-# Loop to print numbers 1 to 99
+
+# Loop to print numbers from 1 to 99
 while [ $i -le 99 ]; do
-    # Check if $i is a valid number
+    # Check if $i is a valid integer
     if ! [[ $i =~ ^[0-9]+$ ]]; then
-    #Print Unexpect value
         echo "Error: Unexpected value for 'i': $i"
         exit 1
     fi
 
-    echo $i
+    echo "$i"
 
     # Increment the counter
-    i=$(($i + 1))
+    i=$((i + 1))
 
     # Check if increment operation was successful
     if [ $? -ne 0 ]; then
-    #Print failed to increment
         echo "Error: Failed to increment the value of 'i'."
         exit 1
     fi
 done
+
 echo "Script executed successfully!!"
