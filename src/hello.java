b657e28ed04433919bff174eb9234160bc58cf07
Shushma-Onefinitylabs <shushma@onefinitylabs.com>
2025-05-26 13:02:25 +0000

    updated

--- src/hello.java
+++ src/hello.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
+        System.out.print("Hello World");
         try {
             printMessage();
         } catch (Exception e) {
