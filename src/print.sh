83220ace3081c7d4fb4e55f210bcaecc75f77f2f
Shushma-Onefinitylabs <shushma@onefinitylabs.com>
2025-05-26 12:07:10 +0000

    updated

--- src/print.sh
+++ src/print.sh
@@ -1,8 +1,5 @@
 # TASK: Print numbers 1 to 99..
 # Customize the plot by adding a title, labels for both axes, and a grid. 
-
-#!/bin/bash
-
 # Check if the required commands are available
 command -v echo >/dev/null 2>&1 || { echo "Error: 'echo' command not found."; exit 1; }
 command -v expr >/dev/null 2>&1 || { echo "Error: 'expr' command not found."; exit 1; }
